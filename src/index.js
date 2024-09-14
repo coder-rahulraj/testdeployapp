df7bfa88acff758e3f706d56cac764925ec0c72e
Rahul Raj <rraj93843@gmail.com>
2024-09-15 00:10:51 +0530

    chnaged index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename='/testdeployapp'>
     <App />
   </BrowserRouter>,
 );
